0389a9df198eaf13550f6203006a9a80454d88fe
Jhonatan <jhonatanlira@gmail.com>
2024-07-08 15:43:59 -0300

    Mockando objetos

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # testesoftwaremockito
+
+Exemplos de utilização do Mockito em projetos Java
+
+- https://www.baeldung.com/mockito-spy
+- https://www.dio.me/articles/mockito-como-utilizar-de-maneira-simples
---
+++ src/test/java/com/dio/mockito/exemplos/CadastrarPessoaTeste.java
@@ -0,0 +1,37 @@
+package com.dio.mockito.exemplos;
+
+import me.dio.mockito.exemplos.ApiDosCorreios;
+import me.dio.mockito.exemplos.CadastrarPessoa;
+import me.dio.mockito.exemplos.DadosLocalizacao;
+import me.dio.mockito.exemplos.Pessoa;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class CadastrarPessoaTeste {
+
+    @Mock
+    private ApiDosCorreios apiDosCorreios;
+    @InjectMocks
+    private CadastrarPessoa cadastrarPessoa;
+
+    @Test
+    void validarDadosDeCadastro(){
+        DadosLocalizacao dadosLocalizacao = new DadosLocalizacao("RJ","Cabo Frio", "Ezio Cardoso da Fonseca","","Jardim Esperança");
+        Mockito.when(apiDosCorreios.buscaDadosComBaseNoCep("28920000")).thenReturn(dadosLocalizacao);
+        Pessoa pessoa = cadastrarPessoa.cadastrarPessoa("Jhonatan", "000000000", LocalDate.now(),"28920000");
+
+            assertEquals("Jhonatan", pessoa.getNome());
+            assertEquals("000000000", pessoa.getDocumento());
+            assertEquals("", pessoa.getEndereco().getComplemento());
+            assertEquals("RJ", pessoa.getEndereco().getUf());
+    }
+}
